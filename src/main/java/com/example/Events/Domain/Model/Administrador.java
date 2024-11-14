7f636c1b7fa488de683e7aae940772d7f1f83592
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-13 21:41:29 -0600

    mapeando clases

--- src/main/java/com/example/Events/Domain/Model/Administrador.java
+++ src/main/java/com/example/Events/Domain/Model/Administrador.java
@@ -1,4 +1,27 @@
 package com.example.Events.Domain.Model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
 public class Administrador {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    @Setter
+    int id;
+
+    @Getter
+    @Setter
+    String rol;
+
+    @Getter
+    @Setter
+    String nombre;
 }
