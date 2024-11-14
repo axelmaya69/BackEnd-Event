c608f2afad1d5a401775ce5287ddce6a3f011448
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-13 21:41:58 -0600

    mapeando clases

--- src/main/java/com/example/Events/Domain/Model/Exponente.java
+++ src/main/java/com/example/Events/Domain/Model/Exponente.java
@@ -1,4 +1,38 @@
 package com.example.Events.Domain.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
 public class Exponente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    @Setter
+    int id;
+
+    @Getter
+    @Setter
+    String nombre;
+
+    @Getter
+    @Setter
+    String apellidoP;
+
+    @Getter
+    @Setter
+    String apellidoM;
+
+    @Getter
+    @Setter
+    String correoElectronico;
+
+    @Getter
+    @Setter
+    String descripcion;
+
 }
