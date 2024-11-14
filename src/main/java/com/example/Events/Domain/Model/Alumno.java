aa285af46ec6fbf459eb810fbfe479d0abb374dc
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-13 21:41:37 -0600

    mapeando clases

--- src/main/java/com/example/Events/Domain/Model/Alumno.java
+++ src/main/java/com/example/Events/Domain/Model/Alumno.java
@@ -1,4 +1,31 @@
 package com.example.Events.Domain.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
 public class Alumno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Setter
+    @Getter
+    int id;
+
+    @Getter
+    @Setter
+    String nombre;
+
+    @Getter
+    @Setter
+    String apellidoP;
+
+    @Getter
+    @Setter
+    String apellidoM;
+
 }
+
