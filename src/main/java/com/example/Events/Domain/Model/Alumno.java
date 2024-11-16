36db1229aa511dd3fa572cd28fc4ccb3287d7a0e
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-15 19:55:36 -0600

    agregadno mapeo

--- src/main/java/com/example/Events/Domain/Model/Alumno.java
+++ src/main/java/com/example/Events/Domain/Model/Alumno.java
@@ -1,11 +1,10 @@
 package com.example.Events.Domain.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 public class Alumno {
@@ -26,6 +25,27 @@ public class Alumno {
     @Getter
     @Setter
     String apellidoM;
+
+    @Getter
+    @Setter
+    @OneToMany(mappedBy = "alumno")
+    private List<AlumnoTaller> AlumTall;
+
+    @Getter
+    @Setter
+    @OneToMany(mappedBy = "alumno")
+    private List<AlumnoConferencia> AlumConf;
+
+    @Getter
+    @Setter
+    @OneToMany(mappedBy = "alumno")
+    private List<Comentario> AlumCom;
+
+    @Getter
+    @Setter
+    @OneToMany(mappedBy = "alumno")
+    private List<MeGusta> AlumMeg;
+
 
 }
 
