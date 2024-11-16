e3b7a208fb35152c91ff0a23b7d743b6b7c516e0
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-15 19:56:00 -0600

    agregadno mapeo

--- src/main/java/com/example/Events/Domain/Model/Exponente.java
+++ src/main/java/com/example/Events/Domain/Model/Exponente.java
@@ -1,11 +1,10 @@
 package com.example.Events.Domain.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 public class Exponente {
@@ -35,4 +34,10 @@ public class Exponente {
     @Setter
     String descripcion;
 
+    @OneToMany(mappedBy = "exponente")
+    private List<Taller> taller;
+
+
+    @OneToMany(mappedBy = "exponente")
+    private List<Conferencia> conferencia;
 }
