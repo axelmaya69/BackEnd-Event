2b23f83a10059f222dc004f3ec555232ad0acbde
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-16 20:50:41 -0600

    agregando

---
+++ src/main/java/com/example/Events/Domain/Model/Escuela.java
@@ -0,0 +1,24 @@
+package com.example.Events.Domain.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+public class Escuela {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    @Setter
+    int id;
+
+    @Getter
+    @Setter
+    String nombre;
+
+
+}
