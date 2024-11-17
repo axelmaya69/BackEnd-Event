c86c8031f7cee65885201f633dbdcef1f5c1a0b7
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-16 20:50:21 -0600

    agregando

---
+++ src/main/java/com/example/Events/Domain/Repository/IEscuela.java
@@ -0,0 +1,8 @@
+package com.example.Events.Domain.Repository;
+
+import com.example.Events.Domain.Model.Escuela;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IEscuela extends JpaRepository<Integer, Escuela> {
+
+}
