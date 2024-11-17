2d7af9f89f7bb9c361718db406c0bfc78705460c
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-16 20:57:30 -0600

    agregando

--- src/main/java/com/example/Events/Domain/Model/InscripcionConferencia.java
+++ src/main/java/com/example/Events/Domain/Model/InscripcionConferencia.java
@@ -3,7 +3,10 @@ package com.example.Events.Domain.Model;
 import jakarta.persistence.Entity;
 
 @Entity
-public class AlumnoConferencia {
+public class InscripcionConferencia {
 
+    int id;
 
+    int id_alumno;
+    int id_conferencia;
 }
