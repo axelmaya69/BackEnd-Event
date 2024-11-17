812ade09377ceacf6992c14d7cde6c48942d9ee0
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-16 20:57:06 -0600

    agregando

--- src/main/java/com/example/Events/Domain/Model/Alumno.java
+++ src/main/java/com/example/Events/Domain/Model/Alumno.java
@@ -35,12 +35,12 @@ public class Alumno {
     @Getter
     @Setter
     @OneToMany(mappedBy = "alumno")
-    private List<AlumnoTaller> AlumTall;
+    private List<InscripcionTaller> AlumTall;
 
     @Getter
     @Setter
     @OneToMany(mappedBy = "alumno")
-    private List<AlumnoConferencia> AlumConf;
+    private List<InscripcionConferencia> AlumConf;
 
     @Getter
     @Setter
