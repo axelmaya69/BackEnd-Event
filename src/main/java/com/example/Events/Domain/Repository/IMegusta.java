f71f537419c9eda88e89c63fe0ac8d61bb2355e7
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-18 20:27:07 -0600

    refactorizando

--- src/main/java/com/example/Events/Domain/Repository/IMegusta.java
+++ src/main/java/com/example/Events/Domain/Repository/IMegusta.java
@@ -1,6 +1,5 @@
 package com.example.Events.Domain.Repository;
 
-import com.example.Events.Domain.Model.MeGusta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IMegusta extends JpaRepository<Integer, MeGusta> {
