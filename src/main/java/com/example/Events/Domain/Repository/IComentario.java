ce5a997729cd5beaf4e30783e5144c5735838ca6
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-18 20:28:38 -0600

    refactorizando

--- src/main/java/com/example/Events/Domain/Repository/IComentario.java
+++
@@ -1,6 +0,0 @@
-package com.example.Events.Domain.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IComentario extends JpaRepository<Integer, Comentario> {
-}
--- src/main/java/com/example/Events/Domain/Repository/IMegusta.java
+++
@@ -1,6 +0,0 @@
-package com.example.Events.Domain.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IMegusta extends JpaRepository<Integer, MeGusta> {
-}
