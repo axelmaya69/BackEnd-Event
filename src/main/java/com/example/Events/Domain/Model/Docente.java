b858139b692fd4a750059658452446851f321932
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-19 20:56:19 -0600

    refactorizando

--- src/main/java/com/example/Events/Domain/Model/Docente.java
+++ src/main/java/com/example/Events/Domain/Model/Docente.java
@@ -1,4 +1,25 @@
 package com.example.Events.Domain.Model;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Docente {
+
+
+    int id;
+
+
+    String nombre;
+
+    String apellidoP;
+
+    String apellidoM;
+
+    int fkConferencia;
+
+    int fkTaller;
+    
+
+
+
 }
