232cf540843cfb6cf86e902f953265348e9692f0
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-20 21:37:17 -0600

    refactorixando

---
+++ src/main/java/com/example/Events/Domain/Model/OtroEvento.java
@@ -0,0 +1,26 @@
+package com.example.Events.Domain.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+public class OtroEvento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    @Setter
+    int id;
+
+    @Getter
+    @Setter
+    String nombre;
+
+    @Getter
+    @Setter
+    String descripcion;
+
+}
