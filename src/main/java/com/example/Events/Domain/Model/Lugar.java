80066084a70f35b304ce23283c89eb477503d1bd
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-20 21:37:06 -0600

    refactorixando

--- src/main/java/com/example/Events/Domain/Model/Lugar.java
+++ src/main/java/com/example/Events/Domain/Model/Lugar.java
@@ -1,18 +1,22 @@
 package com.example.Events.Domain.Model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 public class Lugar {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Getter
     @Setter
     int id;
 
 
     @Getter
-    @Setter
+    @SetterO
     String lugar;
 }
