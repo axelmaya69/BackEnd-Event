9a2052c16edb9b79e8fe94e3c58bb56f69e8ff88
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-22 20:51:57 -0600

    modificaciones

---
+++ src/main/java/com/example/Events/Domain/Repository/IAlumnoConferencia.java
@@ -0,0 +1,7 @@
+package com.example.Events.Domain.Repository;
+
+import com.example.Events.Domain.Model.AlumnoConferencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IAlumnoConferencia extends JpaRepository<Integer, AlumnoConferencia> {
+}
