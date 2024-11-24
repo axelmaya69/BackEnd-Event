c36c9752e1a3d2ba18ee734b44aafd0ca76ad863
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-23 20:40:24 -0600

    refactorizando

--- src/main/java/com/example/Events/Domain/Repository/IDocente.java
+++ src/main/java/com/example/Events/Domain/Repository/IDocente.java
@@ -2,6 +2,8 @@ package com.example.Events.Domain.Repository;
 
 import com.example.Events.Domain.Model.Docente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface IDocente extends JpaRepository<Integer, Docente> {
+@Repository
+public interface IDocente extends JpaRepository<  Docente,Integer> {
 }
