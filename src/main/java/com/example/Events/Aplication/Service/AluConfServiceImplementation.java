88cfa2dba6bc0f7a67fc1cb40fe8fd074d0d5ea3
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-25 21:30:32 -0600

    agregando capa service

--- src/main/java/com/example/Events/Aplication/Service/AluConfServiceImplementation.java
+++ src/main/java/com/example/Events/Aplication/Service/AluConfServiceImplementation.java
@@ -1,4 +1,40 @@
 package com.example.Events.Aplication.Service;
 
-public class AluConfServiceImplementation {
+import com.example.Events.Domain.Model.AlumnoConferencia;
+import com.example.Events.Domain.Service.IAlumnoConferenciaService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AluConfServiceImplementation implements IAlumnoConferenciaService {
+    @Override
+    public AlumnoConferencia crearAlumnoConferencia(AlumnoConferencia alumnoConferencia) {
+        return null;
+    }
+
+    @Override
+    public AlumnoConferencia obtenerAlumnoConferencia(int id) {
+        return null;
+    }
+
+    @Override
+    public List<AlumnoConferencia> obtenerAlumnosConferencias() {
+        return List.of();
+    }
+
+    @Override
+    public List<AlumnoConferencia> obtenerPorNombre(String alumnoConferencia) {
+        return List.of();
+    }
+
+    @Override
+    public AlumnoConferencia actualizarAlumnoConferencia(int id, AlumnoConferencia alumnoConferencia) {
+        return null;
+    }
+
+    @Override
+    public void eliminarAlumnoConferencia(int id) {
+
+    }
 }
