b5d60f96a74e8950cda34ade7c415f7e6636ce97
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-25 21:30:22 -0600

    agregando capa service

--- src/main/java/com/example/Events/Aplication/Service/AdministradorServiceImplementation.java
+++ src/main/java/com/example/Events/Aplication/Service/AdministradorServiceImplementation.java
@@ -1,4 +1,40 @@
 package com.example.Events.Aplication.Service;
 
-public class AdministradorServiceImplementation {
+import com.example.Events.Domain.Model.Administrador;
+import com.example.Events.Domain.Service.IAdministradorService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AdministradorServiceImplementation implements IAdministradorService {
+    @Override
+    public Administrador crearAdministrador(Administrador administrador) {
+        return null;
+    }
+
+    @Override
+    public Administrador obtenerAdministrador(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Administrador> obtenerAdministradores() {
+        return List.of();
+    }
+
+    @Override
+    public List<Administrador> obtenerPorNombre(String administrador) {
+        return List.of();
+    }
+
+    @Override
+    public Administrador actualizarAdministrador(int id, Administrador administrador) {
+        return null;
+    }
+
+    @Override
+    public void eliminarAdministrador(int id) {
+
+    }
 }
