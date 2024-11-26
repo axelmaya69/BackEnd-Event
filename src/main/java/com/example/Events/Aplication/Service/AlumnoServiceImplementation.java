ad40d0c40db5d8b4ccd640a696a865ac5aaba6bd
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-25 21:30:42 -0600

    agregando capa service

--- src/main/java/com/example/Events/Aplication/Service/AlumnoServiceImplementation.java
+++ src/main/java/com/example/Events/Aplication/Service/AlumnoServiceImplementation.java
@@ -1,4 +1,41 @@
 package com.example.Events.Aplication.Service;
 
-public class AlumnoServiceImplementation {
+import com.example.Events.Domain.Model.Alumno;
+import com.example.Events.Domain.Service.IAlumnoService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class AlumnoServiceImplementation implements IAlumnoService {
+    @Override
+    public Alumno crearAlumno(Alumno alumno) {
+        return null;
+    }
+
+    @Override
+    public Alumno obtenerAlumno(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Alumno> obtenerAlumnos() {
+        return List.of();
+    }
+
+    @Override
+    public List<Alumno> obtenerPorNombre(String alumno) {
+        return List.of();
+    }
+
+    @Override
+    public Alumno actualizarAlumno(int id, Alumno alumno) {
+        return null;
+    }
+
+    @Override
+    public void eliminarAlumno(int id) {
+
+    }
 }
