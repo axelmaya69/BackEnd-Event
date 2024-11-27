9c3687800ac8ebd443b205cb417aabe3d986c5eb
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-26 20:29:11 -0600

    inyectando dependiecias

--- src/main/java/com/example/Events/Aplication/Service/AluConfServiceImplementation.java
+++ src/main/java/com/example/Events/Aplication/Service/AluConfServiceImplementation.java
@@ -1,40 +1,58 @@
 package com.example.Events.Aplication.Service;
 
+import com.example.Events.Domain.Model.Administrador;
 import com.example.Events.Domain.Model.AlumnoConferencia;
+import com.example.Events.Domain.Repository.IAlumnoConferencia;
 import com.example.Events.Domain.Service.IAlumnoConferenciaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class AluConfServiceImplementation implements IAlumnoConferenciaService {
+
+    private final IAlumnoConferencia aluconfRep;
+
+    @Autowired
+    public AluConfServiceImplementation(IAlumnoConferencia aluconfRep) {
+        this.aluconfRep = aluconfRep;
+    }
+
+
     @Override
     public AlumnoConferencia crearAlumnoConferencia(AlumnoConferencia alumnoConferencia) {
-        return null;
+        return aluconfRep.save(alumnoConferencia);
     }
 
     @Override
     public AlumnoConferencia obtenerAlumnoConferencia(int id) {
-        return null;
+        return aluconfRep.findById(id).orElse(null);
     }
 
     @Override
     public List<AlumnoConferencia> obtenerAlumnosConferencias() {
-        return List.of();
+        return aluconfRep.findAll();
     }
 
     @Override
     public List<AlumnoConferencia> obtenerPorNombre(String alumnoConferencia) {
-        return List.of();
+        return aluconfRep.findByNombreContaining(alumnoConferencia);
     }
 
     @Override
     public AlumnoConferencia actualizarAlumnoConferencia(int id, AlumnoConferencia alumnoConferencia) {
+        AlumnoConferencia updateAlConf = aluconfRep.findById(id).get();
+        if(aluconfRep.existsById(id)){
+            updateAlConf.setConferencia(alumnoConferencia.getConferencia());
+            updateAlConf.setAlumno(alumnoConferencia.getAlumno());
+            return aluconfRep.save(updateAlConf);
+        }
         return null;
     }
 
     @Override
     public void eliminarAlumnoConferencia(int id) {
-
+    aluconfRep.deleteById(id);
     }
 }
