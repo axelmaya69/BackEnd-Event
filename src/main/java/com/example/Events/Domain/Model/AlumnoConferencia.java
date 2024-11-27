5757b66bb97eea2ed47a6c4df09360606fc07d3d
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-26 20:29:42 -0600

    agregando getter y setters

--- src/main/java/com/example/Events/Domain/Model/AlumnoConferencia.java
+++ src/main/java/com/example/Events/Domain/Model/AlumnoConferencia.java
@@ -1,20 +1,28 @@
 package com.example.Events.Domain.Model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 public class AlumnoConferencia {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    @Setter
     int id;
 
     @ManyToOne
     @JoinColumn(name = "id_alumno")
+    @Getter
+    @Setter
     private Alumno alumno;
 
     @ManyToOne
     @JoinColumn(name = "id_conferencia")
+    @Getter
+    @Setter
     private Conferencia conferencia;
 
 
