b498346505c7eaacaf9384c207d5486c9e6293bc
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-26 20:30:43 -0600

    agregando busqueda por nombre a interfaz

--- src/main/java/com/example/Events/Domain/Repository/IDocenteConferencia.java
+++ src/main/java/com/example/Events/Domain/Repository/IDocenteConferencia.java
@@ -1,9 +1,13 @@
 package com.example.Events.Domain.Repository;
 
+import com.example.Events.Domain.Model.AlumnoConferencia;
 import com.example.Events.Domain.Model.DocenteConferencia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IDocenteConferencia extends JpaRepository< DocenteConferencia , Integer> {
+    List<DocenteConferencia> findByNombreContaining(String docenteConferencia);
 }
