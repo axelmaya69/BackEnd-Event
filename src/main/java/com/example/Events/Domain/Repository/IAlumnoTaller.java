7387060a0177a0c99f866c2d14013095b789c7d8
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-26 20:30:35 -0600

    agregando busqueda por nombre a interfaz

--- src/main/java/com/example/Events/Domain/Repository/IAlumnoTaller.java
+++ src/main/java/com/example/Events/Domain/Repository/IAlumnoTaller.java
@@ -1,9 +1,14 @@
 package com.example.Events.Domain.Repository;
 
+import com.example.Events.Domain.Model.Alumno;
+import com.example.Events.Domain.Model.AlumnoConferencia;
 import com.example.Events.Domain.Model.AlumnoTaller;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IAlumnoTaller extends JpaRepository< AlumnoTaller , Integer> {
+    List<AlumnoTaller> findByNombreContaining(String alumnoTaller);
 }
--- src/main/java/com/example/Events/Domain/Repository/IConferencia.java
+++ src/main/java/com/example/Events/Domain/Repository/IConferencia.java
@@ -1,9 +1,13 @@
 package com.example.Events.Domain.Repository;
 
+import com.example.Events.Domain.Model.AlumnoConferencia;
 import com.example.Events.Domain.Model.Conferencia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IConferencia extends JpaRepository< Conferencia, Integer> {
+    List<Conferencia> findByNombreContaining(String nombreConferencia);
 }
