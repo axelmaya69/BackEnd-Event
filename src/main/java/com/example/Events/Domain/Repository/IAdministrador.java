85326673a6b55000c662ebf7f327d73955a32335
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-26 20:30:03 -0600

    agregando busqueda por nombre a interfaz

--- src/main/java/com/example/Events/Domain/Repository/IAdministrador.java
+++ src/main/java/com/example/Events/Domain/Repository/IAdministrador.java
@@ -4,6 +4,9 @@ import com.example.Events.Domain.Model.Administrador;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IAdministrador extends JpaRepository<Administrador,Integer > {
+    List<Administrador> findByNombreContaining(String nombreAdministrador);
 }
