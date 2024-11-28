77393c7fbeaeee70a6975df0e7779d7e9ceadd9f
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-27 20:45:09 -0600

    configurando service

--- src/main/java/com/example/Events/Aplication/Service/AlumnoServiceImplementation.java
+++ src/main/java/com/example/Events/Aplication/Service/AlumnoServiceImplementation.java
@@ -1,7 +1,9 @@
 package com.example.Events.Aplication.Service;
 
 import com.example.Events.Domain.Model.Alumno;
+import com.example.Events.Domain.Repository.IAlumno;
 import com.example.Events.Domain.Service.IAlumnoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,33 +11,53 @@ import java.util.List;
 
 @Service
 public class AlumnoServiceImplementation implements IAlumnoService {
+
+
+    private final IAlumno aluRepo;
+
+
+    @Autowired
+    public AlumnoServiceImplementation(IAlumno aluRepo) {
+        this.aluRepo = aluRepo;
+    }
+
     @Override
     public Alumno crearAlumno(Alumno alumno) {
-        return null;
+        return aluRepo.save(alumno);
     }
 
     @Override
     public Alumno obtenerAlumno(int id) {
-        return null;
+        return aluRepo.findById(id).orElse(null);
     }
 
     @Override
     public List<Alumno> obtenerAlumnos() {
-        return List.of();
+        return aluRepo.findAll();
     }
 
     @Override
     public List<Alumno> obtenerPorNombre(String alumno) {
-        return List.of();
+        return aluRepo.findByNombreContaining(alumno);
     }
 
     @Override
     public Alumno actualizarAlumno(int id, Alumno alumno) {
+        Alumno updateAlumno = aluRepo.findById(id).get();
+        if(aluRepo.existsById(id)){
+            updateAlumno.setNombre(alumno.getNombre());
+            updateAlumno.setApellidoP(alumno.getApellidoP());
+            updateAlumno.setApellidoM(alumno.getApellidoM());
+            updateAlumno.setNumeroDeControl(alumno.getNumeroDeControl());
+            updateAlumno.setUsuario(alumno.getUsuario());
+            updateAlumno.setPassword(alumno.getPassword());
+        return aluRepo.save(updateAlumno);
+        }
         return null;
     }
 
     @Override
     public void eliminarAlumno(int id) {
-
+    aluRepo.deleteById(id);
     }
 }
