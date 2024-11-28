a025dcca211784e90952163e391ee6213ed4aaae
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-27 20:46:20 -0600

    añadiendo getter y setters

--- src/main/java/com/example/Events/Domain/Model/Docente.java
+++ src/main/java/com/example/Events/Domain/Model/Docente.java
@@ -40,9 +40,13 @@ public class Docente {
     private String password;
 
     @OneToMany(mappedBy = "docente")
+    @Getter
+    @Setter
     private List<DocenteConferencia> docenteConferencia;
 
     @OneToMany(mappedBy = "docente")
+    @Getter
+    @Setter
     private List<DocenteTaller> docenteTaller;
 
 }
