1b9fb47b3715d037bc2f961b5573ee0c215c1ee9
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-27 20:45:18 -0600

    configurando service

--- src/main/java/com/example/Events/Aplication/Service/AluTallServiceImplementation.java
+++ src/main/java/com/example/Events/Aplication/Service/AluTallServiceImplementation.java
@@ -1,4 +1,57 @@
 package com.example.Events.Aplication.Service;
 
-public class AluTallServiceImplementation {
+import com.example.Events.Domain.Model.AlumnoTaller;
+import com.example.Events.Domain.Repository.IAlumnoTaller;
+import com.example.Events.Domain.Service.IAlumnoTallerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AluTallServiceImplementation implements IAlumnoTallerService {
+
+    private final IAlumnoTaller aluTallRep;
+
+    @Autowired
+    public AluTallServiceImplementation(IAlumnoTaller aluTallRep) {
+        this.aluTallRep = aluTallRep;
+    }
+
+
+    @Override
+    public AlumnoTaller crearAlumnoTaller(AlumnoTaller alumnotaller) {
+        return aluTallRep.save(alumnotaller);
+    }
+
+    @Override
+    public AlumnoTaller obtenerAlumnoTaller(int id) {
+        return aluTallRep.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<AlumnoTaller> obtenerAlumnosTalleres() {
+        return aluTallRep.findAll();
+    }
+
+    @Override
+    public List<AlumnoTaller> obtenerPorNombre(String alumnotaller) {
+        return aluTallRep.findByNombreContaining(alumnotaller);
+    }
+
+    @Override
+    public AlumnoTaller actualizarAlumnoTaller(int id, AlumnoTaller alumnotaller) {
+        AlumnoTaller updateAlTall = aluTallRep.findById(id).get();
+        if(aluTallRep.existsById(id)){
+            updateAlTall.setAlumno(alumnotaller.getAlumno());
+            updateAlTall.setTaller(alumnotaller.getTaller());
+            return aluTallRep.save(updateAlTall);
+        }
+        return null;
+    }
+
+    @Override
+    public void eliminarAlumnoTaller(int id) {
+    aluTallRep.deleteById(id);
+    }
 }
