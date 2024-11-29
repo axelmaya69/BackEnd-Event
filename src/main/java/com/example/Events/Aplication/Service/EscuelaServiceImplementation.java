50ab20a9a3b4192b131f060f63cffe69f955b507
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-28 22:10:04 -0600

    modiicando logica api

--- src/main/java/com/example/Events/Aplication/Service/EscuelaServiceImplementation.java
+++ src/main/java/com/example/Events/Aplication/Service/EscuelaServiceImplementation.java
@@ -1,4 +1,56 @@
 package com.example.Events.Aplication.Service;
 
-public class EscuelaServiceImplementation {
+import com.example.Events.Domain.Model.Escuela;
+import com.example.Events.Domain.Repository.IEscuela;
+import com.example.Events.Domain.Service.IEscuelaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EscuelaServiceImplementation implements IEscuelaService {
+
+    private final IEscuela escRep;
+
+    @Autowired
+    public EscuelaServiceImplementation(IEscuela escRep) {
+        this.escRep = escRep;
+    }
+
+
+    @Override
+    public Escuela crearEscuela(Escuela escuela) {
+        return escRep.save(escuela);
+    }
+
+    @Override
+    public Escuela obtenerEscuela(int id) {
+        return escRep.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Escuela> obtenerEscuelas() {
+        return escRep.findAll();
+    }
+
+    @Override
+    public List<Escuela> obtenerPorNombre(String escuela) {
+        return escRep.findByNombreContaining(escuela);
+    }
+
+    @Override
+    public Escuela actualizarEscuela(int id, Escuela escuela) {
+        Escuela updateEscuela = escRep.findById(id).get();
+        if(escRep.existsById(id)){
+            updateEscuela.setNombre(escuela.getNombre());
+            return escRep.save(updateEscuela);
+        }
+        return null;
+    }
+
+    @Override
+    public void eliminarEscuela(int id) {
+    escRep.deleteById(id);
+    }
 }
