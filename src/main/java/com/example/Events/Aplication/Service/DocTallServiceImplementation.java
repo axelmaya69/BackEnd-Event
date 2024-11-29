9b30b849580d907b791ab816fa16e59e55c419e0
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-28 22:09:54 -0600

    modiicando logica api

--- src/main/java/com/example/Events/Aplication/Service/DocTallServiceImplementation.java
+++ src/main/java/com/example/Events/Aplication/Service/DocTallServiceImplementation.java
@@ -1,4 +1,57 @@
 package com.example.Events.Aplication.Service;
 
-public class DocTallServiceImplementation {
+import com.example.Events.Domain.Model.DocenteTaller;
+import com.example.Events.Domain.Repository.IDocenteTaller;
+import com.example.Events.Domain.Service.IDocenteTallerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DocTallServiceImplementation implements IDocenteTallerService {
+
+    private final IDocenteTaller docTallRep;
+
+    @Autowired
+    public DocTallServiceImplementation(IDocenteTaller docTallRep) {
+        this.docTallRep = docTallRep;
+    }
+
+
+    @Override
+    public DocenteTaller crearDocenteTaller(DocenteTaller docenteTaller) {
+        return docTallRep.save(docenteTaller);
+    }
+
+    @Override
+    public DocenteTaller obtenerDocenteTaller(int id) {
+        return docTallRep.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<DocenteTaller> obtenerDocentesTalleres() {
+        return docTallRep.findAll();
+    }
+
+    @Override
+    public List<DocenteTaller> obtenerPorNombre(String docenteTaller) {
+        return docTallRep.findByNombreContaining(docenteTaller);
+    }
+
+    @Override
+    public DocenteTaller actualizarDocenteTaller(int id, DocenteTaller docenteTaller) {
+        DocenteTaller updateDocTall = docTallRep.findById(id).get();
+        if(docTallRep.existsById(id)){
+            updateDocTall.setTaller(docenteTaller.getTaller());
+            updateDocTall.setDocente(docenteTaller.getDocente());
+            return docTallRep.save(updateDocTall);
+        }
+        return null;
+    }
+
+    @Override
+    public void eliminarDocenteTaller(int id) {
+    docTallRep.deleteById(id);
+    }
 }
