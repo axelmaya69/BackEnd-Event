ef343e6edc23b15647a3ccc4a782e92235c50740
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-28 22:09:37 -0600

    modiicando logica api

--- src/main/java/com/example/Events/Aplication/Service/DocConfServiceImplementation.java
+++ src/main/java/com/example/Events/Aplication/Service/DocConfServiceImplementation.java
@@ -1,4 +1,57 @@
 package com.example.Events.Aplication.Service;
 
-public class DocConfServiceImplementation {
+import com.example.Events.Domain.Model.DocenteConferencia;
+import com.example.Events.Domain.Repository.IDocenteConferencia;
+import com.example.Events.Domain.Service.IDocenteConferenciaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DocConfServiceImplementation implements IDocenteConferenciaService {
+
+    private final IDocenteConferencia doConfRepo;
+
+    @Autowired
+    public DocConfServiceImplementation(IDocenteConferencia doConfRepo) {
+        this.doConfRepo = doConfRepo;
+    }
+
+
+    @Override
+    public DocenteConferencia crearDocenteConferencia(DocenteConferencia docenteConferencia) {
+        return doConfRepo.save(docenteConferencia);
+    }
+
+    @Override
+    public DocenteConferencia obtenerDocenteConferencia(int id) {
+        return doConfRepo.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<DocenteConferencia> obtenerDocenteConferencias() {
+        return doConfRepo.findAll();
+    }
+
+    @Override
+    public List<DocenteConferencia> obtenerPorNombre(String docenteConferencia) {
+        return doConfRepo.findByNombreContaining(docenteConferencia);
+    }
+
+    @Override
+    public DocenteConferencia actualizarDocenteConferencia(int id, DocenteConferencia docenteConferencia) {
+        DocenteConferencia updateDocConf = doConfRepo.findById(id).get();
+        if(doConfRepo.existsById(id)){
+            updateDocConf.setDocente(docenteConferencia.getDocente());
+            updateDocConf.setConferencia(docenteConferencia.getConferencia());
+            return doConfRepo.save(updateDocConf);
+        }
+        return null;
+    }
+
+    @Override
+    public void eliminarDocenteConferencia(int id) {
+    doConfRepo.deleteById(id);
+    }
 }
