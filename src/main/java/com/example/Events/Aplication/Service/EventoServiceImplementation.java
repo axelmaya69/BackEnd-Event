77f424a7db504cda4530ca43d2a0b2a81bbe376c
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-28 22:10:13 -0600

    modiicando logica api

--- src/main/java/com/example/Events/Aplication/Service/EventoServiceImplementation.java
+++ src/main/java/com/example/Events/Aplication/Service/EventoServiceImplementation.java
@@ -1,4 +1,58 @@
 package com.example.Events.Aplication.Service;
 
-public class EventoServiceImplementation {
+import com.example.Events.Domain.Model.Evento;
+import com.example.Events.Domain.Repository.IEvento;
+import com.example.Events.Domain.Service.IEventoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EventoServiceImplementation implements IEventoService {
+
+    private final IEvento eventoRep;
+
+    @Autowired
+    public EventoServiceImplementation(IEvento eventoRep) {
+        this.eventoRep = eventoRep;
+    }
+
+
+    @Override
+    public Evento crearEvento(Evento evento) {
+        return eventoRep.save(evento);
+    }
+
+    @Override
+    public Evento obtenerEvento(int id) {
+        return eventoRep.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Evento> obtenerEventos() {
+        return eventoRep.findAll();
+    }
+
+    @Override
+    public List<Evento> obtenerPorNombre(String evento) {
+        return eventoRep.findByNombreContaining(evento);
+    }
+
+    @Override
+    public Evento actualizarEvento(int id, Evento evento) {
+        Evento updateEvento = eventoRep.findById(id).get();
+        if(eventoRep.existsById(id)){
+            updateEvento.setOtrosEventos(evento.getOtrosEventos());
+            updateEvento.setConferencias(evento.getConferencias());
+            updateEvento.setTalleres(evento.getTalleres());
+            return eventoRep.save(updateEvento);
+        }
+        return null;
+    }
+
+    @Override
+    public void eliminarEvento(int id) {
+    eventoRep.deleteById(id);
+    }
 }
