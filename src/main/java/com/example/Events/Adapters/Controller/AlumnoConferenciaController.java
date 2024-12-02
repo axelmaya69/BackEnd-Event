448308d49cec2bf98aa162573e01dd595339b7d0
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-01 20:56:25 -0600

    editando notacion getmapping en clase aluConf

--- src/main/java/com/example/Events/Adapters/Controller/AlumnoConferenciaController.java
+++ src/main/java/com/example/Events/Adapters/Controller/AlumnoConferenciaController.java
@@ -1,4 +1,25 @@
 package com.example.Events.Adapters.Controller;
 
+
+import com.example.Events.Domain.Service.IAlumnoConferenciaService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/AlumnoConferencia")
 public class AlumnoConferenciaController {
+
+    private final IAlumnoConferenciaService alumnoConferenciaService;
+
+
+    public AlumnoConferenciaController(IAlumnoConferenciaService alumnoConferenciaService) {
+        this.alumnoConferenciaService = alumnoConferenciaService;
+    }
+
+    @GetMapping("/get")
+    
+
+
+
 }
