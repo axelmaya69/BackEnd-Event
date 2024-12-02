58b8c161909d6129edc416a2bd56e1a3e7435fc4
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-01 20:58:40 -0600

    editando notacion getmapping en clase aluConf

--- src/main/java/com/example/Events/Adapters/Controller/AlumnoConferenciaController.java
+++ src/main/java/com/example/Events/Adapters/Controller/AlumnoConferenciaController.java
@@ -1,10 +1,16 @@
 package com.example.Events.Adapters.Controller;
 
 
+import com.example.Events.Domain.Model.AlumnoConferencia;
 import com.example.Events.Domain.Service.IAlumnoConferenciaService;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/AlumnoConferencia")
@@ -18,7 +24,19 @@ public class AlumnoConferenciaController {
     }
 
     @GetMapping("/get")
-    
+    public ResponseEntity<?> getAll(){
+        try{
+            List<AlumnoConferencia> aluConf = alumnoConferenciaService.obtenerAlumnosConferencias();
+            return ResponseEntity.ok(aluConf);
+        }
+        catch (DataAccessException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error al acceder a la base de datos, intente más tarde.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error, intentelo más tarde");
+        }
+    }
 
 
 
