b5f694bfa6a5b029cad448cd430c7e44ce50379b
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-02 21:05:00 -0600

    agregando clase controladora alumnocontroller

--- src/main/java/com/example/Events/Adapters/Controller/AlumnoConferenciaController.java
+++ src/main/java/com/example/Events/Adapters/Controller/AlumnoConferenciaController.java
@@ -4,11 +4,10 @@ package com.example.Events.Adapters.Controller;
 import com.example.Events.Domain.Model.AlumnoConferencia;
 import com.example.Events.Domain.Service.IAlumnoConferenciaService;
 import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,7 +36,50 @@ public class AlumnoConferenciaController {
                     .body("Error, intentelo más tarde");
         }
     }
+    @GetMapping("/get/{id}")
+        public ResponseEntity<?> getById(@PathVariable int id){
+            try{
+                AlumnoConferencia getAlumnoConferencia = alumnoConferenciaService.obtenerAlumnoConferencia(id);
+                return ResponseEntity.ok(getAlumnoConferencia);
+        }catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .body("Error, inténtelo más tarde.");
+            }
+    }
+    @PostMapping("/post")
+    public ResponseEntity<?> postAlumnoConferencia(@RequestBody AlumnoConferencia alumnoConferencia){
+        try{
+            AlumnoConferencia postAlumnoConferencia = alumnoConferenciaService.crearAlumnoConferencia(alumnoConferencia);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Creado con éxito");
+        }catch (DataIntegrityViolationException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("El registro ya existe!");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error" +
+                    " inténtelo más tarde");
+        }
+    }
 
+    @PutMapping("/put/{id}")
+    public ResponseEntity<?> putAlumnoConferencia(@PathVariable int id, @RequestBody AlumnoConferencia alumnoConferencia){
+        try{
+            AlumnoConferencia putAlumnoConferencia = alumnoConferenciaService.actualizarAlumnoConferencia(id, alumnoConferencia);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Editado con éxito");
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error" +
+                    " inténtelo más tarde");
+        }
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<String> deleteAlumnoConferencia(@PathVariable int id){
+     try{
+         alumnoConferenciaService.eliminarAlumnoConferencia(id);
+         return ResponseEntity.status(HttpStatus.CREATED).body("Eliminado exitosamente");
+     }catch (Exception e) {
+         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error" +
+                 " inténtelo más tarde.");
+     }
+    }
 
 
 }
