9fdbe7794faa83c800ca322aeb5b7f10762f7feb
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-03 21:37:45 -0600

    Agregando notacion get para clase controladora docenteConferencia

--- src/main/java/com/example/Events/Adapters/Controller/DocenteConferenciaController.java
+++ src/main/java/com/example/Events/Adapters/Controller/DocenteConferenciaController.java
@@ -1,9 +1,16 @@
 package com.example.Events.Adapters.Controller;
 
 
+import com.example.Events.Domain.Model.DocenteConferencia;
 import com.example.Events.Domain.Service.IDocenteConferenciaService;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/docenteConferencia")
@@ -15,5 +22,22 @@ public class DocenteConferenciaController {
     public DocenteConferenciaController(IDocenteConferenciaService docenteConferenciaService) {
         this.docenteConferenciaService = docenteConferenciaService;
     }
-    
+
+    @GetMapping("/get")
+    public ResponseEntity<?> getAll(){
+        try{
+            List<DocenteConferencia> getAll = docenteConferenciaService.obtenerDocenteConferencias();
+            return ResponseEntity.ok(getAll);
+        }
+        catch (DataAccessException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error al acceder a la base de datos, intente más tarde.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error, intentelo más tarde");
+        }
+    }
+
+
+
 }
