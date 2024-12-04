c9a80651698b30e25ef80db24d529d303954ff31
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-03 21:35:56 -0600

    Agregando notaciones y dependencias

--- src/main/java/com/example/Events/Adapters/Controller/DocenteConferenciaController.java
+++ src/main/java/com/example/Events/Adapters/Controller/DocenteConferenciaController.java
@@ -1,4 +1,19 @@
 package com.example.Events.Adapters.Controller;
 
+
+import com.example.Events.Domain.Service.IDocenteConferenciaService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/docenteConferencia")
 public class DocenteConferenciaController {
+
+    private final IDocenteConferenciaService docenteConferenciaService;
+
+
+    public DocenteConferenciaController(IDocenteConferenciaService docenteConferenciaService) {
+        this.docenteConferenciaService = docenteConferenciaService;
+    }
+    
 }
