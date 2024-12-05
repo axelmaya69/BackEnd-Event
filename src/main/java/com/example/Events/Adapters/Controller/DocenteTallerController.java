0ac58ef998c327949e480906e6e9ccd5434cbe62
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-04 21:32:56 -0600

    agregando principales ala clase docenteTallerController

--- src/main/java/com/example/Events/Adapters/Controller/DocenteTallerController.java
+++ src/main/java/com/example/Events/Adapters/Controller/DocenteTallerController.java
@@ -1,4 +1,18 @@
 package com.example.Events.Adapters.Controller;
 
+import com.example.Events.Domain.Repository.IDocenteTaller;
+import com.example.Events.Domain.Service.IDocenteTallerService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/docenteTaller")
 public class DocenteTallerController {
+
+    private final IDocenteTallerService docenteTallerService;
+
+
+    public DocenteTallerController(IDocenteTallerService docenteTallerService) {
+        this.docenteTallerService = docenteTallerService;
+    }
 }
