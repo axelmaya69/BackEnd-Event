3660bf596f770fd5507fc7cbc5de1a4410f87ea4
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-05 20:29:58 -0600

    agregando notacion get  a  docenteController

--- src/main/java/com/example/Events/Adapters/Controller/DocenteTallerController.java
+++ src/main/java/com/example/Events/Adapters/Controller/DocenteTallerController.java
@@ -1,9 +1,16 @@
 package com.example.Events.Adapters.Controller;
 
+import com.example.Events.Domain.Model.DocenteTaller;
 import com.example.Events.Domain.Repository.IDocenteTaller;
 import com.example.Events.Domain.Service.IDocenteTallerService;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/docenteTaller")
@@ -15,4 +22,21 @@ public class DocenteTallerController {
     public DocenteTallerController(IDocenteTallerService docenteTallerService) {
         this.docenteTallerService = docenteTallerService;
     }
+
+    @GetMapping("/get")
+    public ResponseEntity<?> getAll(){
+        try{
+            List<DocenteTaller> getDocentesTall = docenteTallerService.obtenerDocentesTalleres();
+            return ResponseEntity.ok(getDocentesTall);
+        }
+        catch (DataAccessException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error al acceder a la base de datos, intente más tarde.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error, intentelo más tarde");
+        }
+    }
+
+
 }
