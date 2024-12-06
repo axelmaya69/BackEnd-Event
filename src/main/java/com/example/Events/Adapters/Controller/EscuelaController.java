09bc115417b544f8a19d9e2dfa46cd0e680ec217
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-05 20:40:22 -0600

    agregando dependencias necesarias a clase escuelaController

--- src/main/java/com/example/Events/Adapters/Controller/EscuelaController.java
+++ src/main/java/com/example/Events/Adapters/Controller/EscuelaController.java
@@ -1,4 +1,20 @@
 package com.example.Events.Adapters.Controller;
 
+
+import com.example.Events.Domain.Service.IEscuelaService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/escuela")
 public class EscuelaController {
+
+    private final IEscuelaService escuelaService;
+
+
+    public EscuelaController(IEscuelaService escuelaService) {
+        this.escuelaService = escuelaService;
+    }
+
+    
 }
