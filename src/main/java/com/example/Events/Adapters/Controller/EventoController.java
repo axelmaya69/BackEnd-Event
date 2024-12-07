63c09d9fc0d3989797ceefb85c752e4d0e0e1b1f
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-06 19:59:06 -0600

    agregando notacion getAll a clase eventoController

--- src/main/java/com/example/Events/Adapters/Controller/EventoController.java
+++ src/main/java/com/example/Events/Adapters/Controller/EventoController.java
@@ -1,9 +1,16 @@
 package com.example.Events.Adapters.Controller;
 
 
+import com.example.Events.Domain.Model.Evento;
 import com.example.Events.Domain.Service.IEventoService;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/evento")
@@ -15,5 +22,20 @@ public class EventoController {
     public EventoController(IEventoService eventoService) {
         this.eventoService = eventoService;
     }
-    
+
+    @GetMapping("/get")
+    public ResponseEntity<?> getAll(){
+        try{
+            List<Evento> getEventos = eventoService.obtenerEventos();
+            return ResponseEntity.ok(getEventos);
+        }
+        catch (DataAccessException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error al acceder a la base de datos, intente más tarde.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error, intentelo más tarde");
+        }
+    }
+
 }
