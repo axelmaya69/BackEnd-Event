8e95be4adc8948581b926fcc71c294e2f821f644
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-07 19:25:33 -0600

    agregando notacion Get a clase ExponenteController

--- src/main/java/com/example/Events/Adapters/Controller/ExponenteController.java
+++ src/main/java/com/example/Events/Adapters/Controller/ExponenteController.java
@@ -1,9 +1,16 @@
 package com.example.Events.Adapters.Controller;
 
 
+import com.example.Events.Domain.Model.Exponente;
 import com.example.Events.Domain.Service.IExponenteService;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/exponente")
@@ -16,6 +23,21 @@ public class ExponenteController {
         this.exponenteService = exponenteService;
     }
 
-    
+    @GetMapping("/get")
+    public ResponseEntity<?> getAll(){
+        try {
+            List<Exponente> getExponentes = exponenteService.obtenerExponentes();
+            return ResponseEntity.ok(getExponentes);
+        }
+        catch (DataAccessException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error al acceder a la base de datos, intente más tarde.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error, intentelo más tarde");
+        }
+    }
+
+
 
 }
