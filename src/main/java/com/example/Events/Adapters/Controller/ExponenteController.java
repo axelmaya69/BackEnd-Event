348064558977b36e3d678b68488a950e7822ebc3
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-07 19:23:34 -0600

    agregando dependencias neesarias a clase ExponenteController

--- src/main/java/com/example/Events/Adapters/Controller/ExponenteController.java
+++ src/main/java/com/example/Events/Adapters/Controller/ExponenteController.java
@@ -1,4 +1,21 @@
 package com.example.Events.Adapters.Controller;
 
+
+import com.example.Events.Domain.Service.IExponenteService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/exponente")
 public class ExponenteController {
+
+    private final IExponenteService exponenteService;
+
+
+    public ExponenteController(IExponenteService exponenteService) {
+        this.exponenteService = exponenteService;
+    }
+
+    
+
 }
