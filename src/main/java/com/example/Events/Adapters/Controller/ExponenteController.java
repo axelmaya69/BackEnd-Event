34ae0f6bd020c0b62b1e634103fe4a0bb1b2c325
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-07 19:31:51 -0600

    agregando notacion posMapping clase ExponenteController

--- src/main/java/com/example/Events/Adapters/Controller/ExponenteController.java
+++ src/main/java/com/example/Events/Adapters/Controller/ExponenteController.java
@@ -4,6 +4,7 @@ package com.example.Events.Adapters.Controller;
 import com.example.Events.Domain.Model.Exponente;
 import com.example.Events.Domain.Service.IExponenteService;
 import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -59,7 +60,21 @@ public class ExponenteController {
         }
     }
 
-    
+    @PostMapping("/post")
+    public ResponseEntity<?> postExponente(@RequestBody Exponente exponente){
+        try{
+            Exponente postExponente = exponenteService.crearExponente(exponente);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Creado exitosamente");
+        }
+        catch (DataIntegrityViolationException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("El registro ya existe!");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error" +
+                    " inténtelo más tarde");
+        }
+    }
+
+
 
 
 
