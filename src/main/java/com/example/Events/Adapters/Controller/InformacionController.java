ed1f9da19a5bb5054961d8efc039b9d16a90ba06
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-07 19:37:37 -0600

    agregando dependencias neesarias a clase InformacionController

--- src/main/java/com/example/Events/Adapters/Controller/InformacionController.java
+++ src/main/java/com/example/Events/Adapters/Controller/InformacionController.java
@@ -1,4 +1,22 @@
 package com.example.Events.Adapters.Controller;
 
+
+import com.example.Events.Domain.Service.IInformacionService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/informacionController")
 public class InformacionController {
+
+    private final IInformacionService informacionService;
+
+    public InformacionController(IInformacionService informacionService) {
+        this.informacionService = informacionService;
+    }
+
+    
+
+
+
 }
