b99e52f32402812318da492e7995c0964edfece5
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-08 20:49:35 -0600

    agregando notacion getByNameMapping clase LugarController

--- src/main/java/com/example/Events/Adapters/Controller/LugarController.java
+++ src/main/java/com/example/Events/Adapters/Controller/LugarController.java
@@ -6,10 +6,7 @@ import com.example.Events.Domain.Service.ILugarService;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -51,7 +48,16 @@ public class LugarController {
         }
     }
 
-    
+    @GetMapping("/getByName")
+    public ResponseEntity<?> getByName(@RequestParam String nombreLugar){
+        try{
+            List<Lugar> getLugaresByName = lugarService.obtenerPorNombre(nombreLugar);
+            return ResponseEntity.ok(getLugaresByName);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error, inténtelo más tarde.");
+        }
+    }
 
 
 }
