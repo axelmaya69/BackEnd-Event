5dbdfa6dc10e13b3e7f0d9ca175e453eecdfe453
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-08 20:46:02 -0600

    agregando notacion getMapping clase LugarController

--- src/main/java/com/example/Events/Adapters/Controller/LugarController.java
+++ src/main/java/com/example/Events/Adapters/Controller/LugarController.java
@@ -1,9 +1,16 @@
 package com.example.Events.Adapters.Controller;
 
 
+import com.example.Events.Domain.Model.Lugar;
 import com.example.Events.Domain.Service.ILugarService;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/lugar")
@@ -16,5 +23,22 @@ public class LugarController {
         this.lugarService = lugarService;
     }
 
-    
+    @GetMapping("/get")
+    public ResponseEntity<?> getAll(){
+        try{
+            List<Lugar> getAllLugares = lugarService.obtenerLugares();
+            return ResponseEntity.ok(getAllLugares);
+        }
+        catch (DataAccessException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error al acceder a la base de datos, intente más tarde.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error, intentelo más tarde");
+        }
+    }
+        
+
+
+
 }
