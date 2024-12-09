5ba1da6a2c06215ee68004aa0d19b23aaccad36b
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-08 20:52:39 -0600

    agregando notacion postMapping clase LugarController

--- src/main/java/com/example/Events/Adapters/Controller/LugarController.java
+++ src/main/java/com/example/Events/Adapters/Controller/LugarController.java
@@ -4,6 +4,7 @@ package com.example.Events.Adapters.Controller;
 import com.example.Events.Domain.Model.Lugar;
 import com.example.Events.Domain.Service.ILugarService;
 import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -56,6 +57,20 @@ public class LugarController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Error, inténtelo más tarde.");
+        }
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<?> postLugar(@RequestBody Lugar lugar){
+        try{
+            Lugar postLugar = lugarService.crearLugar(lugar);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Creado exitosamente");
+        }
+        catch (DataIntegrityViolationException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("El registro ya existe!");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error" +
+                    " inténtelo más tarde");
         }
     }
 
