0e1a6f373a51e1a9f7abb32e69fdfc4f7c9e9d3e
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-08 20:44:22 -0600

    agregando dependencias neesarias a clase LugarController

--- src/main/java/com/example/Events/Adapters/Controller/LugarController.java
+++ src/main/java/com/example/Events/Adapters/Controller/LugarController.java
@@ -1,4 +1,20 @@
 package com.example.Events.Adapters.Controller;
 
+
+import com.example.Events.Domain.Service.ILugarService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/lugar")
 public class LugarController {
+
+    private final ILugarService lugarService;
+
+
+    public LugarController(ILugarService lugarService) {
+        this.lugarService = lugarService;
+    }
+
+    
 }
