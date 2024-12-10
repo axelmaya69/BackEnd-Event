99ef3fe47f853d6e559a9020547956685a603718
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-09 20:20:20 -0600

    agregando notacion getMapping clase LugarController

--- src/main/java/com/example/Events/Adapters/Controller/OtroEventoController.java
+++ src/main/java/com/example/Events/Adapters/Controller/OtroEventoController.java
@@ -1,8 +1,15 @@
 package com.example.Events.Adapters.Controller;
 
+import com.example.Events.Domain.Model.OtroEvento;
 import com.example.Events.Domain.Service.IOtroEventoService;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/otroEvento")
@@ -15,5 +22,22 @@ public class OtroEventoController {
         this.otroEventoService = otroEventoService;
     }
 
+    @GetMapping("/get")
+    public ResponseEntity<?> getAll(){
+        try{
+            List<OtroEvento> getAllOEv = otroEventoService.obtenerOtrosEventos();
+            return ResponseEntity.ok(getAllOEv);
+        }
+        catch (DataAccessException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error al acceder a la base de datos, intente más tarde.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error, intentelo más tarde");
+        }
+    }
+
     
+
+
 }
