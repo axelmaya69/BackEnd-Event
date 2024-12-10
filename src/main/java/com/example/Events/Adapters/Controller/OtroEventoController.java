80bd9dd9bc22fccf7dfdc0c3c788c52799be4642
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-09 20:18:36 -0600

    agregando dependencias neesarias a clase OtroEventoController

--- src/main/java/com/example/Events/Adapters/Controller/OtroEventoController.java
+++ src/main/java/com/example/Events/Adapters/Controller/OtroEventoController.java
@@ -1,4 +1,19 @@
 package com.example.Events.Adapters.Controller;
 
+import com.example.Events.Domain.Service.IOtroEventoService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/otroEvento")
 public class OtroEventoController {
+
+    private final IOtroEventoService otroEventoService;
+
+
+    public OtroEventoController(IOtroEventoService otroEventoService) {
+        this.otroEventoService = otroEventoService;
+    }
+
+    
 }
