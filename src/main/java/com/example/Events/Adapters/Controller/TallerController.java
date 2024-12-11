dac6355523a5ebfe2534868ffeaebd56c3dc7851
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-10 23:16:13 -0600

    agregando notaciones a clase tallerController

--- src/main/java/com/example/Events/Adapters/Controller/TallerController.java
+++ src/main/java/com/example/Events/Adapters/Controller/TallerController.java
@@ -1,4 +1,11 @@
 package com.example.Events.Adapters.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/taller")
 public class TallerController {
+
+        
 }
