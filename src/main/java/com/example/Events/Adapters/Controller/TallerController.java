95e31121bcb14afdea37b3136cc07bdbf2adbfeb
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-10 23:17:06 -0600

    instanciando interfaz a tallerController

--- src/main/java/com/example/Events/Adapters/Controller/TallerController.java
+++ src/main/java/com/example/Events/Adapters/Controller/TallerController.java
@@ -1,5 +1,6 @@
 package com.example.Events.Adapters.Controller;
 
+import com.example.Events.Domain.Service.ITallerService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/taller")
 public class TallerController {
 
-        
+    private final ITallerService tallerService;
+
+    public TallerController(ITallerService tallerService) {
+        this.tallerService = tallerService;
+    }
 }
