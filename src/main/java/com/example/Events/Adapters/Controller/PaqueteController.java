61712e29a181d6b3e9d7c0f749911c4337639ffc
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-10 23:07:59 -0600

    agregando notacion GetMappingByName a clase paqueteController

--- src/main/java/com/example/Events/Adapters/Controller/PaqueteController.java
+++ src/main/java/com/example/Events/Adapters/Controller/PaqueteController.java
@@ -6,10 +6,7 @@ import com.example.Events.Domain.Service.IPaqueteService;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -45,6 +42,18 @@ public class PaqueteController {
         try{
             Paquete getPaquete = paqueteService.obtenerPaquete(id);
             return ResponseEntity.ok(getPaquete);
+        }
+        catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error, inténtelo más tarde.");
+        }
+    }
+
+    @GetMapping("/getByName")
+    public ResponseEntity<?> getByName(@RequestParam String nombrePaquete){
+        try{
+            List<Paquete> getPaqueteName = paqueteService.obtenerPorNombre(nombrePaquete);
+            return ResponseEntity.ok(getPaqueteName);
         }
         catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
