c374fe80400b1b442783505cfed4973676fadd58
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-10 23:02:40 -0600

    agregando dependencias necesarias a clase paqueteController

--- src/main/java/com/example/Events/Adapters/Controller/PaqueteController.java
+++ src/main/java/com/example/Events/Adapters/Controller/PaqueteController.java
@@ -1,4 +1,21 @@
 package com.example.Events.Adapters.Controller;
 
+
+import com.example.Events.Domain.Service.IPaqueteService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/paquete")
+@RestController
 public class PaqueteController {
+
+    private final IPaqueteService paqueteService;
+
+
+    public PaqueteController(IPaqueteService paqueteService) {
+        this.paqueteService = paqueteService;
+    }
+
+    
+
 }
