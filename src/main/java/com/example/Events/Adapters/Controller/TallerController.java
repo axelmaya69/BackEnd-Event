4bf1a2fc99f12ce35943f807639eccc235c39ac9
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-11 19:16:51 -0600

    agregando notacion Get a clase tallerController

--- src/main/java/com/example/Events/Adapters/Controller/TallerController.java
+++ src/main/java/com/example/Events/Adapters/Controller/TallerController.java
@@ -1,8 +1,15 @@
 package com.example.Events.Adapters.Controller;
 
+import com.example.Events.Domain.Model.Taller;
 import com.example.Events.Domain.Service.ITallerService;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/taller")
@@ -13,4 +20,26 @@ public class TallerController {
     public TallerController(ITallerService tallerService) {
         this.tallerService = tallerService;
     }
+
+    @GetMapping("/get")
+    public ResponseEntity<?> getAll(){
+        try{
+            List<Taller> getTalleres = tallerService.obtenerTalleres();
+            return ResponseEntity.ok(getTalleres);
+        }
+        catch (DataAccessException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error al acceder a la base de datos, intente más tarde.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error, intentelo más tarde");
+        }
+    }
+    
+
+
+
+
+
 }
+
