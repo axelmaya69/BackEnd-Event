9005af61f8df4be538a7aecad422b41c7ce5dd94
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-13 22:38:53 -0600

    agregando clase para crear jwt

---
+++ src/main/java/com/example/Events/Configuration/Security/JwtUtils.java
@@ -0,0 +1,22 @@
+package com.example.Events.Configuration.Security;
+
+import com.example.Events.Domain.Model.Alumno;
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+
+
+    public String generarJWT(String usuario){
+    return Jwts.builder()
+            .setSubject(usuario)
+            .setIssuedAt(new Date())
+            .setExpiration(new Date(new Date().getTime())+)
+    }
+
+
+}
