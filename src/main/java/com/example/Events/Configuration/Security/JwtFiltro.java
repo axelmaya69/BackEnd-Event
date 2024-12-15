a8f1b90b46db0d42d31e2ac15237392f4547e1e1
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-14 19:28:24 -0600

    configurando parametros

--- src/main/java/com/example/Events/Configuration/Security/JwtFiltro.java
+++ src/main/java/com/example/Events/Configuration/Security/JwtFiltro.java
@@ -1,5 +1,30 @@
 package com.example.Events.Configuration.Security;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-public class JwtFiltro {
+import java.io.IOException;
+
+@Component
+public class JwtFiltro extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
 }
