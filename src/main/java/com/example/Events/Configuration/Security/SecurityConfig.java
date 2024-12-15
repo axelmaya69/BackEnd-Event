76ae87423f7ae1700ea3fd9a57ba6678d06f9ad5
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-14 19:42:33 -0600

    configurando parametros

--- src/main/java/com/example/Events/Configuration/Security/SecurityConfig.java
+++ src/main/java/com/example/Events/Configuration/Security/SecurityConfig.java
@@ -1,5 +1,36 @@
 package com.example.Events.Configuration.Security;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-    
+@Configuration
 public class SecurityConfig {
+
+    private  final JwtFiltro jwtFiltro;
+
+    public SecurityConfig(JwtFiltro jwtFiltro) {
+        this.jwtFiltro = jwtFiltro;
+    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/auth/**").permitAll() // Endpoints públicos
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtFiltro, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
 }
