bfabce98a0fc770c68fdbd972ff539cfac955173
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-16 21:15:16 -0600

    configurando parametros spring security

--- src/main/java/com/example/Events/Configuration/Security/SecurityConfig.java
+++ src/main/java/com/example/Events/Configuration/Security/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests()
+                .requestMatchers("/blog/**").permitAll()
 
     }
 
