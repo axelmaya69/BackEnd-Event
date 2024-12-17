f922fcc5c758e175a14e3e905a07c54c73871934
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-16 21:18:58 -0600

    configurando NUEVA FORMA ACEPTADA DE HACERLO EN spring security

--- src/main/java/com/example/Events/Configuration/Security/SecurityConfig.java
+++ src/main/java/com/example/Events/Configuration/Security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.Events.Configuration.Security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -23,16 +24,18 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests()
-                .requestMatchers("/blog/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .rememberMe();
-        
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/blog/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .rememberMe(Customizer.withDefaults());
+
+        return http.build();
+
     }
 
     @Bean
