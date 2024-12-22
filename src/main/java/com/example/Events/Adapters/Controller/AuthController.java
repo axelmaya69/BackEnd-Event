2476d4c92cd800d79244cf82c30be4ddabdfa97b
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-21 22:14:15 -0600

    agregando configuraciones e seguridad

--- src/main/java/com/example/Events/Adapters/Controller/AuthController.java
+++ src/main/java/com/example/Events/Adapters/Controller/AuthController.java
@@ -1,4 +1,9 @@
 package com.example.Events.Adapters.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
 }
