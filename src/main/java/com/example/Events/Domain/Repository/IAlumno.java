d7e714ae465ab3c90480e7ea75eb4969d137b3e0
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-21 22:11:43 -0600

    agregando configuraciones e seguridad

--- src/main/java/com/example/Events/Domain/Repository/IAlumno.java
+++ src/main/java/com/example/Events/Domain/Repository/IAlumno.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface IAlumno extends JpaRepository< Alumno, Integer> {
     List<Alumno> findByNombreContaining(String nombreAlumno);
     Optional<Alumno> findByUsuario(String usuario);
 }
+
