855eac4e97079a055dbcb2da1ffd8435e4af5b54
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-22 21:45:09 -0600

    configuradcion a clase security

--- src/main/java/com/example/Events/Configuration/Security/SecurityConfig.java
+++ src/main/java/com/example/Events/Configuration/Security/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/blog/**").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(formLogin -> formLogin
