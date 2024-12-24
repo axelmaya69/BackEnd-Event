7ae255b01003a91d68831b8c869a480b2c0eb14d
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-24 17:04:16 -0600

    refactorizando codigo

--- src/main/java/com/example/Events/Domain/Model/APersona.java
+++ src/main/java/com/example/Events/Domain/Model/APersona.java
@@ -2,10 +2,16 @@ package com.example.Events.Domain.Model;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 public abstract class APersona {
 
+    @Id
+    @Getter
+    @Setter
     int id;
 
     String nombre;
