e9ecc9d1bcb86e84ebddfdf76d572c35f2e0271c
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-24 17:09:32 -0600

    creando clase abstracta para heredar a alumno y docente

--- src/main/java/com/example/Events/Domain/Model/APersona.java
+++ src/main/java/com/example/Events/Domain/Model/APersona.java
@@ -2,6 +2,8 @@ package com.example.Events.Domain.Model;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,6 +14,7 @@ public abstract class APersona {
     @Id
     @Getter
     @Setter
+    @GeneratedValue(strategy = GenerationType.AUTO)
     int id;
 
     @Getter
