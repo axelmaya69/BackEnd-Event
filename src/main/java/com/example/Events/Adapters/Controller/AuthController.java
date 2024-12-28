d4e267f35f9fe886181239a98eed091882f2e118
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-27 20:04:56 -0600

    implementando postmapping

--- src/main/java/com/example/Events/Adapters/Controller/AuthController.java
+++ src/main/java/com/example/Events/Adapters/Controller/AuthController.java
@@ -2,6 +2,10 @@ package com.example.Events.Adapters.Controller;
 
 
 import lombok.RequiredArgsConstructor;
+import org.antlr.v4.runtime.Token;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
+
+    //private service AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<TokenResponse> register(@RequestBody final RegisterRequest request){
+        final TokenResponse token = service.register(request);
+        return ResponseEntity.ok(token);
+    }
+
 }
