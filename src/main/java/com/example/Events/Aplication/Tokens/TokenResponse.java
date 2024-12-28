60862ffdb3f1028ef4301589d323ee86e3fc973a
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-27 20:18:57 -0600

    cambios

--- src/main/java/com/example/Events/Aplication/Tokens/TokenResponse.java
+++ src/main/java/com/example/Events/Aplication/Tokens/TokenResponse.java
@@ -3,7 +3,7 @@ package com.example.Events.Aplication.Tokens;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record TokenResponse(
-    @JsonProperty("acces_token")
+    @JsonProperty("access_token")
     String access_token,
     @JsonProperty("refresh_token")
     String refresh_token
