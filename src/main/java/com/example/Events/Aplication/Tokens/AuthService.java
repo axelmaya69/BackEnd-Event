54c55b6755e262b3ebe089c819d794e92e90cd1a
axelmaya69 <axeldbmaya96@gmail.com>
2024-12-29 21:38:45 -0600

    llenando clase

--- src/main/java/com/example/Events/Aplication/Tokens/AuthService.java
+++ src/main/java/com/example/Events/Aplication/Tokens/AuthService.java
@@ -1,4 +1,12 @@
 package com.example.Events.Aplication.Tokens;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+
+    private final TokenRepository tokenRepository;
+    
 }
